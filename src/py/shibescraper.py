ba40754595f08f17ace1d178219482a691a09ee9
Nicholas Caley <potatoz@gmail.com>
2014-05-20 13:36:58 -0400

    Update shibescraper.py
    Updated to grab images straight off imgur

--- src/py/shibescraper.py
+++ src/py/shibescraper.py
@@ -1,36 +1,49 @@
 import urllib2
-import pickle
-from bs4 import BeautifulSoup
 
-imgSuffixes = ["jpg", "jpeg", "png", "gif"]
-sites = [
-    'http://reddit.com/r/supershibe',
-    'http://reddit.com/r/shibe',
-    'http://reddit.com/r/doge'
-]
+from bs4 import BeautifulSoup as bs
+from random import shuffle
 
-def parseImgLinks(soup, links):
-    for link in soup.find_all('a'):
-        l = link.get('class')
-        if not l is None and 'title' in l:
-            if any(s in link.get('href') for s in imgSuffixes):
-                links.append(link)
+def scrape_site(url):
+    result = []
+    soup = bs(urllib2.urlopen(url).read())
+    for img in soup.find_all('img'):
+        if img.has_attr('alt'):
+            src = img.get('src')
+            link = ''.join(['http:', src[:src.rindex('.')-1],
+                    src[src.rindex('.'):]])
+            title = img.get('title')
+            if title is None:
+                title = ''
+            else:
+                title = title[:title.index('<p>')]
+            result.append((title, link))
+    return result
 
+def write_html(filename, links):
+    towrite = ''
+    for (title, link) in links:
+        s = u'<br> <p>{}</p><img src="{}" style="max-width: 100%;"></img><br>'.format(title, link)
+        towrite = ''.join([towrite, s])
+    with open(filename, 'w') as f:
+        f.write(towrite.encode('utf8'))
 
-allLinks = []
-for site in sites:
-    soup = BeautifulSoup(urllib2.urlopen(site).read())
-    parseImgLinks(soup, allLinks)
+def main():
 
-allLinks = set(allLinks)
+    sites = [
+        'http://imgur.com/r/shibe',
+        'http://imgur.com/r/supershibe',
+        'http://imgur.com/r/dogecoin',
+        'http://imgur.com/r/doge',
+        'http://imgur.com/r/shiba'
+#    'http://shibe-doge.tumblr.com'
+    ]
 
-f = open('links.html', 'w')
+    filename = 'link2.html'
+    r = []
+    for site in sites:
+        r = r + scrape_site(site)
+        shuffle(r)
+        write_html(filename, r)
 
-for l in allLinks:
-    f.write(str(l) + '<br/>')
-    src = l.get('href')
-    f.write('<img src="' + str(src) + '"/><br/><br/>')
-f.close()
-
-print 'done.'
-
+if __name__ == '__main__':
+    main()
